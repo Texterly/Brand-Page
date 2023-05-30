572261c04a5354ac7a39573a49fcf26c302bea70
Texterly <88582978+Texterly@users.noreply.github.com>
2023-05-30 22:18:23 +0300

    create nav

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 function App() {
   return (
     <div>
@@ -5,12 +7,15 @@ function App() {
         <div className="logo">
           <img src="/images/brand_logo.png" alt="logo" />
         </div>
+
         <ul>
           <li href="#">Menu</li>
           <li href="#">Location</li>
           <li href="#">About</li>
           <li href="#">Contact</li>
         </ul>
+
+        <button>login</button>
       </nav>
     </div>
   );
