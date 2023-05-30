ee4a11b2333aa3234c1cb34258e47c3882ae192c
Texterly <88582978+Texterly@users.noreply.github.com>
2023-05-30 22:03:06 +0300

    add logo

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
