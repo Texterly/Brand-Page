d2c7d807b8c8a047c8e670fd62d251f9cfa44dfb
Texterly <88582978+Texterly@users.noreply.github.com>
2023-06-03 11:03:47 +0300

    add navigation component

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,22 +1,10 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 }
