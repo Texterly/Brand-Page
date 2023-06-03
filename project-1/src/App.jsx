8abec509873b1eed1ca9dba1ed5ecfbcf2851022
Texterly <88582978+Texterly@users.noreply.github.com>
2023-06-03 12:41:02 +0300

    add hero section

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/HeroSection";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Hero />
     </div>
   );
 }
